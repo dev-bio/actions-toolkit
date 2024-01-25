16250b6044dc145e1bdd21a9828ef3c88baa7d01
André Storfjord Kristiansen <a@dev.bio>
2024-01-25 15:56:32 +0100

    Event Method
    Method to determine whether event is bot generated.

--- src/client/models/events/mod.rs
+++ src/client/models/events/mod.rs
@@ -22,3 +22,29 @@ pub enum Event {
     #[serde(rename = "schedule")]
     Schedule,
 }
+
+impl Event {
+    pub fn is_bot_generated(&self) -> bool {
+        match self {
+            Event::IssueComment(comment) => match comment {
+                EventIssueComment::Deleted { comment, .. } |
+                EventIssueComment::Created { comment, .. } |
+                EventIssueComment::Edited { comment, .. } => {
+                    comment.get_author()
+                        .is_bot()
+                },
+            },
+            Event::Issue(issue) => match issue {
+                EventIssue::Reopened { issue, .. } |
+                EventIssue::Deleted { issue, .. } |
+                EventIssue::Opened { issue, .. } |
+                EventIssue::Closed { issue, .. } |
+                EventIssue::Edited { issue, .. } => {
+                    issue.get_author()
+                        .is_bot()
+                },
+            },
+            _ => false,
+        }
+    }
+}
